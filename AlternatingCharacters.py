bb61bc7a9601d54abb67a5be683a5ce29d67e0eb
SriRawin <srirawin1510@gmail.com>
2021-08-19 03:03:39 +0530

    Alternating Characters problem

---
+++ AlternatingCharacters.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    deletion_count=0
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            deletion_count+=1
+    return deletion_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
