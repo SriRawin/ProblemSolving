5170da71ddf3c5cb4f94e25be5677fce9856dd36
SriRawin <srirawin1510@gmail.com>
2021-08-19 02:40:19 +0530

    Sherlock and squares problem

---
+++ SherlockandSquares.py
@@ -0,0 +1,43 @@
+import math
+import os
+import random
+import re
+import sys
+from typing import Counter
+
+#
+# Complete the 'squares' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER a
+#  2. INTEGER b
+#
+
+def squares(a, b):
+    square_count=math.ceil(math.sqrt(b)) -  math.floor(math.sqrt(a))
+    
+   
+    
+    return square_count
+    
+    
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        a = int(first_multiple_input[0])
+
+        b = int(first_multiple_input[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
