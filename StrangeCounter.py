e1832ad7b2956b491efe63bebe0a5a828925418e
SriRawin <srirawin1510@gmail.com>
2021-09-19 09:18:51 +0530

    StrangeCounter problem

---
+++ StrangeCounter.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'strangeCounter' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER t as parameter.
+#
+
+
+def strangeCounter(t):
+    rem = 3
+    while t > rem:
+        t = t-rem
+        rem *= 2
+    print(rem-t+1)
+    return rem-t+1
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
