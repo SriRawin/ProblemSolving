a6192cccbdb132589e7d6d0f86b8895fbb5f1e44
SriRawin <srirawin1510@gmail.com>
2021-10-11 12:26:55 +0530

    Delete Duplicates in a linked list problem

---
+++ DeleteDuplicatesLinkedList.py
@@ -0,0 +1,72 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+def removeDuplicates(llist):
+    current_node = llist
+    next_node = llist.next
+    while current_node and next_node:
+        if current_node.data == next_node.data:
+            current_node.next = next_node.next
+            next_node = current_node.next
+        if (current_node and next_node) and (current_node.data != next_node.data):
+            current_node = next_node
+            next_node = next_node.next
+    return llist
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = removeDuplicates(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
