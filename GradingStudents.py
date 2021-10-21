a85776893dc71af71eaec679f11ebf751959a49f
SriRawin <srirawin1510@gmail.com>
2021-10-21 19:21:28 +0530

    Grading Students problem

---
+++ GradingStudents.py
@@ -0,0 +1,38 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    result_list=[]
+    for grade in grades:
+        if grade>=38 and abs(grade - (math.ceil(grade/5)*5))<3:
+            grade=math.ceil(grade/5)*5
+        result_list.append(grade)
+    return result_list
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
