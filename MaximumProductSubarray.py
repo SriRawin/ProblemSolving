35351a2d208a760502e77e6dd539835dfe3fae52
SriRawin <srirawin1510@gmail.com>
2022-01-17 20:10:05 +0530

    Maximum sub-array product problem

---
+++ MaximumProductSubarray.py
@@ -0,0 +1,20 @@
+def maxProduct(nums):
+    result = max(nums)
+    max_product = 1
+    min_product = 1
+    for num in nums:
+        if num == 0:
+            max_product = min_product = 1
+            continue
+        curr = max_product*num
+        max_product = max(num, curr, min_product*num)
+        print("max", max_product)
+        min_product = min(num, curr, min_product*num)
+        print("min", min_product)
+        result = max(result, max_product)
+    return result
+
+
+nums = [-1, -2, -3]
+result = maxProduct(nums)
+print(result)
