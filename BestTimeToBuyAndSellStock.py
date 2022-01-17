6b97773088b36fb7a55706a59e3327f650b71a6c
SriRawin <srirawin1510@gmail.com>
2022-01-17 16:09:16 +0530

    Best time to buy and sell stocks problem

--- BestTimeToBuyAndSellStock.py
+++ BestTimeToBuyAndSellStock.py
@@ -1,22 +1,16 @@
-prices = [7, 1, 5, 3, 6, 4]
-profit = 0
-decreasing = False
-increasing = False
-inStock = False
-for i in range(0, len(prices)):
-    if i+1 < len(prices) and prices[i] < prices[i+1]:
-        decreasing = False
-        increasing = True
-    if i+1 < len(prices) and prices[i] > prices[i+1]:
-        increasing = False
-        decreasing = True
-    if decreasing or i == len(prices)-1:
-        if inStock:
-            profit += prices[i]-buy
-            inStock = False
-    if increasing:
-        if not inStock:
-            buy = prices[i]
-            inStock = True
+def maxProfit(prices):
+    buy = 0
+    sell = 1
+    curr_max = 0
+    while sell < len(prices):
+        if prices[buy] < prices[sell]:
+            curr_max = max(curr_max, prices[sell]-prices[buy])
+        else:
+            buy = sell
+        sell += 1
+    return curr_max
 
-print(profit)
+
+prices = [2, 1, 4]
+result = maxProfit(prices)
+print(result)
