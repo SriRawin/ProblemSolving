544e06cf9dc8ede59a108b03e01754e775641a0f
SriRawin <srirawin1510@gmail.com>
2022-01-18 19:19:50 +0530

    Search in rotated array problem

---
+++ SearchInRotatedArray.py
@@ -0,0 +1,25 @@
+def search(nums, target):
+    start = 0
+    end = len(nums)-1
+    while start <= end:
+        mid = (start+end)//2
+        if nums[mid] == target:
+            return mid
+        if nums[start] <= nums[mid]:
+            if target > nums[mid] or target < nums[start]:
+                start = mid+1
+            else:
+                end = mid-1
+        else:
+            if target < nums[mid] or target > nums[end]:
+                end = mid-1
+            else:
+                start = mid+1
+
+    return -1
+
+
+nums = [6, 0, 1, 2, 3, 4]
+for num in nums:
+    result = search(nums, num)
+    print(result)
