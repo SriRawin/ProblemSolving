5fc6ad724b77a280ee956dcd4e62586c7d95d2c6
SriRawin <srirawin1510@gmail.com>
2022-02-07 09:37:55 +0530

    Valid palindrome problem

--- Blind75/ValidPalindrome.py
+++ Blind75/ValidPalindrome.py
@@ -3,18 +3,17 @@ def isPalindrome(s):
     r = len(s)-1
     s = s.lower()
     while l < r:
-        while l < len(s)-1 and not s[l].isalnum():
+        while l < r and not s[l].isalnum():
             l += 1
-        while r > 0 and not s[r].isalnum():
+        while r > l and not s[r].isalnum():
             r -= 1
-        if s[l].isalnum() and s[r].isalnum():
-            if s[l] != s[r]:
-                return False
+        if s[l] != s[r]:
+            return False
         l += 1
         r -= 1
     return True
 
 
-s = "race a car"
+s = ".,"
 res = isPalindrome(s)
 print(res)
